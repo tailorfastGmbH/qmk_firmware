0cfa2e97c4b99a73ba97679a56ad52b515038e1f
Sacha Dolski <sacha.dolski@prolutions.ch>
2024-08-16 12:58:16 +0200

    feat: add keymap via-dos with tapdance and windows mac shortcut overrides

--- keyboards/nuphy/air75_v2/ansi/ansi.c
+++ keyboards/nuphy/air75_v2/ansi/ansi.c
@@ -420,7 +420,7 @@ void timer_pro(void) {
  * @brief  londing eeprom data.
  */
 void londing_eeprom_data(void) {
-    eeconfig_read_kb_datablock(&user_config);
+    eeconfig_read_user_datablock(&user_config);
     if (user_config.default_brightness_flag != 0xA5) {
         /* first power on, set rgb matrix brightness at middle level*/
         rgb_matrix_sethsv(255, 255, RGB_MATRIX_MAXIMUM_BRIGHTNESS - RGB_MATRIX_VAL_STEP * 2);
