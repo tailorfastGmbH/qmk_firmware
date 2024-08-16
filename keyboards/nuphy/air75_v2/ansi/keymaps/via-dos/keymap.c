d9653d76830ff0b5f83d35ee1b59643e6d72f1fd
Sacha Dolski <sacha.dolski@prolutions.ch>
2024-08-16 15:12:28 +0200

    add ctrl+x

--- keyboards/nuphy/air75_v2/ansi/keymaps/via-dos/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/via-dos/keymap.c
@@ -39,6 +39,7 @@ tap_dance_action_t tap_dance_actions[] = {
 // 1 = LAYER 0, means MAC
 const key_override_t ctrl_c_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_C, G(KC_C), 1);
 const key_override_t ctrl_v_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_V, G(KC_V), 1);
+const key_override_t ctrl_x_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_X, G(KC_X), 1);
 const key_override_t ctrl_z_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_Z, G(KC_Z), 1);
 const key_override_t ctrl_f_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_F, G(KC_F), 1);
 const key_override_t ctrl_a_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_A, G(KC_A), 1);
@@ -47,6 +48,7 @@ const key_override_t ctrl_a_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC
 const key_override_t **key_overrides = (const key_override_t *[]){
 	&ctrl_c_key_override,
 	&ctrl_v_key_override,
+	&ctrl_x_key_override,
 	&ctrl_z_key_override,
 	&ctrl_f_key_override,
 	&ctrl_a_key_override,
@@ -67,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, 			KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,		KC_RBRC, 				KC_BSLS,	KC_PGDN,
 	KC_LCTL,			KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	 						KC_ENT,		KC_HOME,
 	TD(TD_LSHIFT_CAPS),	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,  TD(TD_RSHIFT_CAPS),	KC_UP,		KC_END,
-	KC_LCTL,	KC_LALT,	KC_LGUI,										KC_SPC, 							KC_RALT,	MO(1),   		KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
+	KC_LALT,	KC_LGUI,	KC_RALT,										KC_SPC, 							KC_RALT,	MO(1),   		KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer Mac Fn
 [1] = LAYOUT_ansi_84(
@@ -85,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, 			KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 				KC_BSLS,	KC_PGDN,
 	KC_LCTL,			KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	 						KC_ENT,		KC_HOME,
 	TD(TD_LSHIFT_CAPS),	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,		TD(TD_RSHIFT_CAPS),	    KC_UP,	KC_END,
-	KC_LCTL,	KC_LALT,	KC_LGUI,										KC_SPC, 							KC_RALT,	MO(3),   	KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
+	KC_LALT,	KC_LGUI,	KC_RALT,										KC_SPC, 							KC_RALT,	MO(3),   	KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer win Fn
 [3] = LAYOUT_ansi_84(
@@ -108,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer 5 numbers
 [5] = LAYOUT_ansi_84(
-	TD(TD_ESC_NUMBERS), 			KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_PSCR,	KC_INS,		KC_DEL,
-	KC_GRV, 			KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 				KC_BSPC,	KC_PGUP,
-	KC_TAB, 			_______,  	_______,	_______, 	_______,  	_______,   	_______,   	KC_4,   	KC_5,   	KC_6,  		_______,   	KC_LBRC,	KC_RBRC, 				KC_BSLS,	KC_PGDN,
-	KC_LCTL,			_______,   	_______,   	_______,  	_______,   	_______,  	_______,  	KC_1,   	KC_2,   	KC_3,  		KC_SCLN,	KC_QUOT, 	 						KC_ENT,		KC_HOME,
-	TD(TD_LSHIFT_CAPS),	_______,  	_______,   	_______,  	_______,   	_______,   	_______,	KC_0,      	KC_COMM,	KC_DOT,		KC_SLSH,		TD(TD_RSHIFT_CAPS),	    KC_UP,	KC_END,
-	KC_LCTL,	KC_LALT,	KC_LGUI,										KC_SPC, 							KC_RALT,	MO(3),   	KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
+	TD(TD_ESC_NUMBERS), 	_______,  	_______,	_______, 	_______,  	_______,   	_______,  	_______,  	_______,	_______, 	_______,  	_______,   	_______, 	_______,	_______,	_______,
+	_______, 	_______,  	_______,	_______, 	_______,  	_______,   	_______,	KC_7,   	KC_8,   	KC_9,  		_______,	_______,	_______, 				_______,	_______,
+	_______, 	_______,  	_______,	_______, 	_______,  	_______,   	_______,   	KC_4,   	KC_5,   	KC_6,  		_______,   	_______, 	_______, 				_______,	_______,
+	_______,	_______,   	_______,   	_______,  	_______,   	_______,  	_______,  	KC_1,   	KC_2,   	KC_3,  		_______, 	_______, 	 						_______,	_______,
+	_______,			  	_______,   	_______,  	_______,   	_______,   	_______,	KC_0,      	_______,	_______, 	_______,	_______,			   	_______,	KC_UP,		_______,
+	_______,	_______,	_______,										_______, 							_______,	_______, 	_______,				KC_LEFT,	KC_DOWN,    KC_RGHT),
 };
