aa9cc66862a70ae14d9ac4c44721ad40f0d15583
Florian Huonder <florian.huonder@tailorfast.com>
2024-08-22 11:24:35 +0200

    feat: switch altl and ctrll

--- keyboards/nuphy/air96_v2/ansi/keymaps/via-huf/keymap.c
+++ keyboards/nuphy/air96_v2/ansi/keymaps/via-huf/keymap.c
@@ -40,7 +40,8 @@ const key_override_t ctrl_v_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC
 const key_override_t ctrl_z_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_Z, G(KC_Z), 1);
 const key_override_t ctrl_f_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_F, G(KC_F), 1);
 const key_override_t ctrl_a_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_A, G(KC_A), 1);
-const key_override_t ctrl_d_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_D, G(KC_D), 1); // duplicate line
+const key_override_t ctrl_d_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_D, G(KC_D), 1);
+
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -57,7 +58,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 //Keep num lock always on
 bool led_update_user(led_t led_state) {
     if (!led_state.num_lock) {
-		tap_code(KC_NUM_LOCK);
+		tap_code(KC_NUM_LOCK); //enable num lock
 	}
 	return true;
 }
@@ -71,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_PSCR,	KC_DEL,		KC_HOME,	KC_END,		KC_PGUP,	KC_PGDN,
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,				KC_INS,		KC_PSLS,	KC_PAST,	KC_PMNS,
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,				KC_P7,		KC_P8,		KC_P9,		KC_PPLS,
-	KC_LCTL,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	KC_ENT,								KC_P4,		KC_P5,		KC_P6,
+	KC_LALT,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	KC_ENT,								KC_P4,		KC_P5,		KC_P6,
 	TD(TD_LEFT_SHIFT_CAPS),				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	TD(TD_RIGHT_SHIFT_CAPS),				KC_UP,		KC_P1,		KC_P2,		KC_P3,		KC_PENT,
-	KC_LALT,	KC_LGUI,	KC_RALT,										KC_SPC, 							KC_RALT,	MO(1),   	KC_RCTL,							KC_LEFT,	KC_DOWN,    KC_RGHT,	KC_P0,		KC_PDOT),
+	KC_LCTL,	KC_LGUI,	KC_RALT,										KC_SPC, 							KC_RALT,	MO(1),   	KC_RCTL,							KC_LEFT,	KC_DOWN,    KC_RGHT,	KC_P0,		KC_PDOT),
 // layer 1 Mac Fn
 [1] = LAYOUT(
 	_______, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	MAC_SEARCH, MAC_VOICE,  MAC_DND,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	MAC_PRT,	KC_INS,		_______,	_______,	_______,	_______,
@@ -87,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_PSCR,	KC_DEL,		KC_HOME,	KC_END,		KC_PGUP,	KC_PGDN,
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,				KC_INS,		KC_PSLS,	KC_PAST,	KC_PMNS,
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,				KC_P7,		KC_P8,		KC_P9,		KC_PPLS,
-	KC_LCTL,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	KC_ENT,								KC_P4,		KC_P5,		KC_P6,
+	KC_LALT,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	KC_ENT,								KC_P4,		KC_P5,		KC_P6,
 	TD(TD_LEFT_SHIFT_CAPS),				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	TD(TD_RIGHT_SHIFT_CAPS),				KC_UP,		KC_P1,		KC_P2,		KC_P3,		KC_PENT,
-	KC_LALT,	KC_LGUI,	KC_RALT,										KC_SPC, 							KC_RALT,	MO(3),   	KC_RCTL,							KC_LEFT,	KC_DOWN,    KC_RGHT,	KC_P0,		KC_PDOT),
+	KC_LCTL,	KC_LGUI,	KC_RALT,										KC_SPC, 							KC_RALT,	MO(3),   	KC_RCTL,							KC_LEFT,	KC_DOWN,    KC_RGHT,	KC_P0,		KC_PDOT),
 // layer 3 win Fn
 [3] = LAYOUT(
 	_______, 	KC_BRID,   	KC_BRIU,    _______,  	_______,   	_______,   	_______,   	KC_MPRV,   	KC_MPLY,   	KC_MNXT,  	KC_MUTE, 	KC_VOLD, 	KC_VOLU,	MAC_PRTA,	KC_INS,		_______,	_______,	_______,	_______,
